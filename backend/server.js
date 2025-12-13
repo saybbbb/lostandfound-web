0bdaead98e72fbcca83d3f8b15c705d8a5bd516f
Storm <kjialopier@gmail.com>
2025-12-13 15:44:28 +0800

    Change server.js cors code

--- backend/server.js
+++ backend/server.js
@@ -21,11 +21,10 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
   })
 );
-
-// IMPORTANT: explicitly handle preflight
-app.options("*", cors());
 
 /* ======================================================
    MIDDLEWARE
